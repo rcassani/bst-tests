26376ad5758687afe3bb175674f4f308abe646b6
rcassani <raymundo.cassani@gmail.com>
2023-08-17 18:02:30 -0400

    Add functional test_brainstorm.m

---
+++ test_brainstorm.m
@@ -0,0 +1,78 @@
+function test_brainstorm(bstTestName, bstUserName)
+% TEST_BRAINSTORM Test Brainstorm by running tutorial scripts from scratch
+%
+% USAGE: test_brainstorm(testName, brainstormUsername)
+%
+% INPUTS:
+%    - bstTestName     : Test to run, usually a script in the ./toolbox/scripts folder
+%    - bstUserName  : Cell array of signals {[nSignals1, nSamples1], [nSignals2, nSamples2], ...}
+%
+% For a given bstTestName, the script does:
+%    1. Download tutorial data (if needed)
+%    2. Run tutorial script
+%    3. Send report by email to bstUserName
+%
+% @=============================================================================
+% 
+% Copyright (c) University of Southern California & McGill University
+% This software is distributed under the terms of the GNU General Public License
+% as published by the Free Software Foundation. Further details on the GPLv3
+% license can be found at http://www.gnu.org/copyleft/gpl.html.
+% 
+% FOR RESEARCH PURPOSES ONLY. THE SOFTWARE IS PROVIDED "AS IS," AND THE
+% UNIVERSITY OF SOUTHERN CALIFORNIA AND ITS COLLABORATORS DO NOT MAKE ANY
+% WARRANTY, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO WARRANTIES OF
+% MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, NOR DO THEY ASSUME ANY
+% LIABILITY OR RESPONSIBILITY FOR THE USE OF THIS SOFTWARE.
+%
+% For more information type "brainstorm license" at command prompt.
+% =============================================================================@
+%
+% Authors: Raymundo Cassani, 2023
+
+% Parameters 
+if nargin < 2 || isempty(bstUserName)
+    bstUserName = '';
+end
+
+% Check that Brainstorm is run with local database
+bstDbDir = bst_get('BrainstormDbDir');
+if isempty(regexp(bstDbDir, 'local_db$', 'once'))
+    warning('Run this script with brainstorm using local db');
+    return
+end
+
+% Make a clean directory for data
+tmpDir = bst_fullfile(pwd, 'tmpdir');
+if (exist(tmpDir, 'dir') == 7)
+    file_delete(tmpDir, 1, 3);
+end
+mkdir(tmpDir);
+% Brainstorm FTP server
+bstFtp = ftp('neuroimage.usc.edu');
+
+switch bstTestName
+    case 'tutorial_introduction'        
+        % Downlaod and unzip data        
+        tmpFtpFile = mget(bstFtp, '/pub/tutorials/sample_introduction.zip', tmpDir);
+        bst_unzip(tmpFtpFile{:}, tmpDir);
+        tutorial_introduction(tmpDir);
+         
+    case 'tutorial_connectivity'
+        tutorial_connectivity();
+                
+end
+
+% Sending email
+% Process: Send report by email
+bst_process('CallProcess', 'process_report_email', [], [], ...
+    'username',   bstUserName, ...
+    'cc',         '', ...
+    'subject',    ['Test: ' bstTestName ' completed'], ...
+    'reportfile', 'last', ...
+    'full',       1);
+
+% Stop Brainstorm
+brainstorm stop
+% Quit Matlab
+exit
