bc97f892cf71c1cb371bef70f87fd868bb47c811
rcassani <raymundo.cassani@gmail.com>
2023-08-31 21:09:10 -0400

    Add: Other tutorials

--- test_brainstorm.m
+++ test_brainstorm.m
@@ -61,15 +61,105 @@ mkdir(tmpDir);
 bstFtp = ftp('neuroimage.usc.edu');
 
 switch bstTestName
+
     case 'tutorial_introduction'        
-        % Downlaod and unzip data        
-        tmpFtpFile = mget(bstFtp, '/pub/tutorials/sample_introduction.zip', tmpDir);
-        bst_unzip(tmpFtpFile{:}, tmpDir);
+        tmpDwnFile = mget(bstFtp, '/pub/tutorials/sample_introduction.zip', tmpDir);
+        bst_unzip(tmpDwnFile{:}, tmpDir);
         tutorial_introduction(tmpDir);
-         
+
     case 'tutorial_connectivity'
         tutorial_connectivity();
-                
+
+    case 'tutorial_coherence'
+        tmpDwnFile = bst_fullfile(tmpDir, 'SubjectCMC.zip');
+        bst_websave(tmpDwnFile, 'https://download.fieldtriptoolbox.org/tutorial/SubjectCMC.zip');
+        bst_unzip(tmpDwnFile, bst_fullfile(tmpDir, 'SubjectCMC'));
+        tutorial_coherence(tmpDir);
+
+    case 'tutorial_ephys'
+        tmpDwnFile = mget(bstFtp, '/pub/tutorials/sample_ephys.zip', tmpDir);
+        bst_unzip(tmpDwnFile{:}, tmpDir);
+        tutorial_ephys(tmpDir);
+
+    case 'tutorial_epilepsy'
+        tmpDwnFile = mget(bstFtp, '/pub/tutorials/sample_epilepsy.zip', tmpDir);
+        bst_unzip(tmpDwnFile{:}, tmpDir);
+        tutorial_epilepsy(tmpDir);
+
+    case 'tutorial_epileptogenicity'
+        tmpDwnFile = mget(bstFtp, '/pub/tutorials/tutorial_epimap_bids.zip', tmpDir);
+        bst_unzip(tmpDwnFile{:}, tmpDir);
+        tutorial_epileptogenicity(tmpDir);
+
+%     case 'tutorial_fem_charm'
+%         % REQUIRES TO INSTALL SimNIBS
+%         tmpDwnFile = mget(bstFtp, '/pub/tutorials/sample_fem.zip', tmpDir);
+%         bst_unzip(tmpDwnFile{:}, tmpDir);
+%         tutorial_fem_charm(tmpDir);
+
+    case 'tutorial_fem_tensors'
+        tmpDwnFile = bst_fullfile(tmpDir, 'BrainSuiteTutorialSVReg.zip');
+        bst_websave(tmpDwnFile, 'http://brainsuite.org/WebTutorialData/BrainSuiteTutorialSVReg_Sept16.zip');
+        bst_unzip(tmpDwnFile, tmpDir);
+        tmpDwnFile = bst_fullfile(tmpDir, 'DWI.zip');
+        bst_websave(tmpDwnFile, 'http://brainsuite.org/WebTutorialData/DWI_Feb15.zip');
+        bst_unzip(tmpDwnFile, tmpDir);
+        tutorial_fem_tensors(tmpDir);
+
+%     case 'tutorial_frontiers2018'
+%         % REQUIRES TO DOWNLOAD ~100GB https://openneuro.org/datasets/ds000117
+%         tutorial_frontiers2018(tmpDir);
+
+%     case 'tutorial_visual'
+%         % REQUIRES TO DOWNLOAD ~100GB https://openneuro.org/datasets/ds000117
+%         tutorial_visual(tmpDir);
+
+%     case 'tutorial_hcp'
+%         % REQUIRES TO DOWNLOAD ~20GB  HCP-MEG2 distribution: subject #175237
+%         tutorial_hcp(tmpDir);
+
+    case 'tutorial_neuromag'
+        tmpDwnFile = mget(bstFtp, '/pub/tutorials/sample_neuromag.zip', tmpDir);
+        bst_unzip(tmpDwnFile{:}, tmpDir);
+        tutorial_neuromag(tmpDir);
+
+%     case 'tutorial_omega'
+% %         % REQUIRES TO DOWNLOAD ~12GB  https://openneuro.org/datasets/ds000247
+%         tutorial_omega(tmpDir);
+
+    case 'tutorial_phantom_ctf'
+        tmpDwnFile = mget(bstFtp, '/pub/tutorials/sample_phantom_ctf.zip', tmpDir);
+        bst_unzip(tmpDwnFile{:}, tmpDir);
+        tutorial_phantom_ctf(tmpDir);
+
+    case 'tutorial_phantom_elekta'
+        tmpDwnFile = mget(bstFtp, '/pub/tutorials/sample_phantom_elekta.zip', tmpDir);
+        bst_unzip(tmpDwnFile{:}, tmpDir);
+        tutorial_phantom_elekta(tmpDir);
+
+    case 'tutorial_practicalmeeg'
+        tmpDwnFile = mget(bstFtp, '/pub/tutorials/tutorial_practicalmeeg.zip', tmpDir);
+        bst_unzip(tmpDwnFile{:}, tmpDir);
+        tutorial_practicalmeeg(bst_fullfile(tmpDir, 'tutorial_practicalmeeg'));
+
+    case 'tutorial_raw'
+        tmpDwnFile = mget(bstFtp, '/pub/tutorials/sample_raw.zip', tmpDir);
+        bst_unzip(tmpDwnFile{:}, tmpDir);
+        tutorial_raw(tmpDir);
+
+    case 'tutorial_resting'
+        tmpDwnFile = mget(bstFtp, '/pub/tutorials/sample_resting.zip', tmpDir);
+        bst_unzip(tmpDwnFile{:}, tmpDir);
+        tutorial_resting(tmpDir);
+
+    case 'tutorial_simulations'
+        tutorial_simulations();
+
+    case 'tutorial_yokogawa'
+        tmpDwnFile = mget(bstFtp, '/pub/tutorials/sample_yokogawa.zip', tmpDir);
+        bst_unzip(tmpDwnFile{:}, tmpDir);
+        tutorial_yokogawa(tmpDir);
+
 end
 
 % Host info
