e19d9b7810a9763d2753a883da407e0b5519620b
rcassani <raymundo.cassani@gmail.com>
2024-09-23 10:23:09 -0400

    Allow downloading data directly from server, with BST user and pwd

--- test_brainstorm.m
+++ test_brainstorm.m
@@ -1,14 +1,19 @@
-function test_brainstorm(bstTestName, bstUserName)
+function test_brainstorm(tutorialNames, dataDir, reportDir, bstUser, bstPwd)
 % TEST_BRAINSTORM Test Brainstorm by running tutorial scripts from scratch
+%               - It requires tor run Brainstorm with 'local' database
+%               - 'test_brainstorm.m' should in the same dir as 'brainstorm.m'
 %
-% USAGE: test_brainstorm(testName, brainstormUsername)
+% USAGE: test_brainstorm(tutorialNames, dataDir, reportDir, bstUser, bstPwd)
 %
 % INPUTS:
-%    - bstTestName  : Test to run, usually a script in the ./toolbox/scripts folder
-%    - bstUserName  : Cell array of signals {[nSignals1, nSamples1], [nSignals2, nSamples2], ...}
+%    - tutorialNames : Tutorial or {Tutorials} to run, usually scripts in "./toolbox/scripts"
+%    - dataDir       : (opt) Directory wtih tutorial data files               (default = 'pwd'/tmpdir)
+%    - reportDir     : (opt) Directory to save reports                        (default = reports are not saved)
+%    - bstUser       : (opt) BST user to receive email with report            (default = no email)
+%    - bstPwd        : (opt) Password for BST user to download data if needed (default = empty)
 %
-% For a given bstTestName, the script does:
-%    1. Download tutorial data (if needed)
+% For a given tutorial in 'tutorialNames', the script does:
+%    1. Find/get the tutorial data (download if bstUser and bstPwd are available)
 %    2. Run tutorial script
 %    3. Send report by email to bstUserName
 %
@@ -28,20 +33,66 @@ function test_brainstorm(bstTestName, bstUserName)
 % For more information type "brainstorm license" at command prompt.
 % =============================================================================@
 %
-% Authors: Raymundo Cassani, 2023
+% Authors: Raymundo Cassani, 2023-2024
 
-% Parameters 
-if nargin < 2 || isempty(bstUserName)
-    bstUserName = '';
+
+%% ===== PARAMETERS =====
+if nargin < 2 || isempty(dataDir)
+    dataDir = bst_fullfile(pwd, 'tmpdir');
+end
+if nargin < 3 || isempty(reportDir)
+    reportDir = '';
+end
+if nargin < 4 || isempty(bstUser)
+    bstUser = '';
+end
+if nargin < 4 || isempty(bstPwd)
+    bstPwd = '';
 end
 
-% Add path 'bst-tests' with support functions
-% 'bst-tests' and 'brainstorm3' are expected at the same level
-bstTestsDir = fullfile(fileparts(bst_get('BrainstormHomeDir')), 'bst-tests');
-if (exist(bstTestsDir, 'dir') == 7)
-    addpath(bstTestsDir);
-else
-    error('Dir ''bst-tests'' should be placed at the same level as ''brainstorm3'' ');
+% All tutorials
+if ischar(tutorialNames)
+    if strcmpi(tutorialNames, 'all')
+        tutorialNames = {'tutorial_introduction', ...
+                         'tutorial_connectivity', ...
+                         'tutorial_coherence', ...
+                         'tutorial_ephys', ...
+                         'tutorial_dba', ...
+                         'tutorial_epilepsy', ...
+                         'tutorial_epileptogenicity', ...
+                         'tutorial_fem_charm', ...
+                         'tutorial_fem_tensors', ...
+                         'tutorial_frontiers2018', ...
+                         'tutorial_visual', ...
+                         'tutorial_hcp', ...
+                         'tutorial_neuromag', ...
+                         'tutorial_omega', ...
+                         'tutorial_phantom_ctf', ...
+                         'tutorial_phantom_elekta', ...
+                         'tutorial_practicalmeeg', ...
+                         'tutorial_raw', ...
+                         'tutorial_resting', ...
+                         'tutorial_simulations', ...
+                         'tutorial_yokogawa', ...
+                    };
+    else
+        tutorialNames = {tutorialNames};
+    end
+end
+
+
+%% ===== START BRAINSTORM =====
+% Check that Brainstorm is in the Matlab path
+res = exist('brainstorm.m', 'file');
+if res ~=2
+    error('Could not find "brainstorm.m" in Matlab path.');
+end
+
+% Start Brainstorm without GUI and with local database
+stopBstAtEnd = 0;
+if ~brainstorm('status')
+    brainstorm nogui local
+    stopBstAtEnd = 1;
 end
 
 % Check that Brainstorm is run with local database
@@ -51,130 +102,250 @@ if isempty(regexp(bstDbDir, 'local_db$', 'once'))
     return
 end
 
-% Make a clean directory for data
-tmpDir = bst_fullfile(pwd, 'tmpdir');
-if (exist(tmpDir, 'dir') == 7)
-    file_delete(tmpDir, 1, 3);
+
+%% ===== DATA AND REPORT DIRS =====
+% Data directory
+if ~exist(dataDir, 'dir')
+    mkdir(dataDir);
 end
-mkdir(tmpDir);
-% Brainstorm FTP server
-bstFtp = ftp('neuroimage.usc.edu');
-
-switch bstTestName
-
-    case 'tutorial_introduction'        
-        tmpDwnFile = mget(bstFtp, '/pub/tutorials/sample_introduction.zip', tmpDir);
-        bst_unzip(tmpDwnFile{:}, tmpDir);
-        tutorial_introduction(tmpDir);
-
-    case 'tutorial_connectivity'
-        tutorial_connectivity();
-
-    case 'tutorial_coherence'
-        tmpDwnFile = bst_fullfile(tmpDir, 'SubjectCMC.zip');
-        bst_websave(tmpDwnFile, 'https://download.fieldtriptoolbox.org/tutorial/SubjectCMC.zip');
-        bst_unzip(tmpDwnFile, bst_fullfile(tmpDir, 'SubjectCMC'));
-        tutorial_coherence(tmpDir);
-
-    case 'tutorial_ephys'
-        tmpDwnFile = mget(bstFtp, '/pub/tutorials/sample_ephys.zip', tmpDir);
-        bst_unzip(tmpDwnFile{:}, tmpDir);
-        tutorial_ephys(tmpDir);
-
-    case 'tutorial_epilepsy'
-        tmpDwnFile = mget(bstFtp, '/pub/tutorials/sample_epilepsy.zip', tmpDir);
-        bst_unzip(tmpDwnFile{:}, tmpDir);
-        tutorial_epilepsy(tmpDir);
-
-    case 'tutorial_epileptogenicity'
-        tmpDwnFile = mget(bstFtp, '/pub/tutorials/tutorial_epimap_bids.zip', tmpDir);
-        bst_unzip(tmpDwnFile{:}, tmpDir);
-        tutorial_epileptogenicity(tmpDir);
-
-%     case 'tutorial_fem_charm'
-%         % REQUIRES TO INSTALL SimNIBS
-%         tmpDwnFile = mget(bstFtp, '/pub/tutorials/sample_fem.zip', tmpDir);
-%         bst_unzip(tmpDwnFile{:}, tmpDir);
-%         tutorial_fem_charm(tmpDir);
-
-    case 'tutorial_fem_tensors'
-        tmpDwnFile = bst_fullfile(tmpDir, 'BrainSuiteTutorialSVReg.zip');
-        bst_websave(tmpDwnFile, 'http://brainsuite.org/WebTutorialData/BrainSuiteTutorialSVReg_Sept16.zip');
-        bst_unzip(tmpDwnFile, tmpDir);
-        tmpDwnFile = bst_fullfile(tmpDir, 'DWI.zip');
-        bst_websave(tmpDwnFile, 'http://brainsuite.org/WebTutorialData/DWI_Feb15.zip');
-        bst_unzip(tmpDwnFile, tmpDir);
-        tutorial_fem_tensors(tmpDir);
-
-%     case 'tutorial_frontiers2018'
-%         % REQUIRES TO DOWNLOAD ~100GB https://openneuro.org/datasets/ds000117
-%         tutorial_frontiers2018(tmpDir);
-
-%     case 'tutorial_visual'
-%         % REQUIRES TO DOWNLOAD ~100GB https://openneuro.org/datasets/ds000117
-%         tutorial_visual(tmpDir);
-
-%     case 'tutorial_hcp'
-%         % REQUIRES TO DOWNLOAD ~20GB  HCP-MEG2 distribution: subject #175237
-%         tutorial_hcp(tmpDir);
-
-    case 'tutorial_neuromag'
-        tmpDwnFile = mget(bstFtp, '/pub/tutorials/sample_neuromag.zip', tmpDir);
-        bst_unzip(tmpDwnFile{:}, tmpDir);
-        tutorial_neuromag(tmpDir);
-
-%     case 'tutorial_omega'
-% %         % REQUIRES TO DOWNLOAD ~12GB  https://openneuro.org/datasets/ds000247
-%         tutorial_omega(tmpDir);
-
-    case 'tutorial_phantom_ctf'
-        tmpDwnFile = mget(bstFtp, '/pub/tutorials/sample_phantom_ctf.zip', tmpDir);
-        bst_unzip(tmpDwnFile{:}, tmpDir);
-        tutorial_phantom_ctf(tmpDir);
-
-    case 'tutorial_phantom_elekta'
-        tmpDwnFile = mget(bstFtp, '/pub/tutorials/sample_phantom_elekta.zip', tmpDir);
-        bst_unzip(tmpDwnFile{:}, tmpDir);
-        tutorial_phantom_elekta(tmpDir);
-
-    case 'tutorial_practicalmeeg'
-        tmpDwnFile = mget(bstFtp, '/pub/tutorials/tutorial_practicalmeeg.zip', tmpDir);
-        bst_unzip(tmpDwnFile{:}, tmpDir);
-        tutorial_practicalmeeg(bst_fullfile(tmpDir, 'tutorial_practicalmeeg'));
-
-    case 'tutorial_raw'
-        tmpDwnFile = mget(bstFtp, '/pub/tutorials/sample_raw.zip', tmpDir);
-        bst_unzip(tmpDwnFile{:}, tmpDir);
-        tutorial_raw(tmpDir);
-
-    case 'tutorial_resting'
-        tmpDwnFile = mget(bstFtp, '/pub/tutorials/sample_resting.zip', tmpDir);
-        bst_unzip(tmpDwnFile{:}, tmpDir);
-        tutorial_resting(tmpDir);
-
-    case 'tutorial_simulations'
-        tutorial_simulations();
-
-    case 'tutorial_yokogawa'
-        tmpDwnFile = mget(bstFtp, '/pub/tutorials/sample_yokogawa.zip', tmpDir);
-        bst_unzip(tmpDwnFile{:}, tmpDir);
-        tutorial_yokogawa(tmpDir);
-
+% Report dir
+if ~isempty(reportDir) && ~exist(reportDir, 'dir')
+    mkdir(reportDir)
 end
 
-% Host info
-hostInfo = test_hostinfo();
 
-% Sending email
-% Process: Send report by email
-bst_process('CallProcess', 'process_report_email', [], [], ...
-    'username',   bstUserName, ...
-    'cc',         '', ...
-    'subject',    ['[Completed] ' bstTestName ' @ ' hostInfo], ...
-    'reportfile', 'last', ...
-    'full',       1);
+%% ===== RUN TUTORIALS, SAVE REPORTS AND SEND EMAIL =====
+for iTutorial = 1 : length(tutorialNames)
+    tutoriallName = tutorialNames{iTutorial};
+    % Clean report history
+    bst_report('ClearHistory', 0);
+    infoStr = 'Error preparing file for tutorial';
+    % === Run tutorial
+    switch tutoriallName
+        case 'tutorial_introduction'
+            dataFile = getTutorialFile(dataDir, 'sample_introduction.zip', bstUser, bstPwd);
+            if exist(dataFile, 'file')
+                bst_unzip(dataFile, dataDir);
+                tutorial_introduction(dataDir);
+            end
+
+        case 'tutorial_connectivity'
+            tutorial_connectivity();
+
+        case 'tutorial_coherence'
+            dataFile = bst_fullfile(dataDir, 'SubjectCMC.zip');
+            if ~exist(dataFile, 'file')
+                bst_websave(dataFile, 'https://download.fieldtriptoolbox.org/tutorial/SubjectCMC.zip');
+            end
+            if exist(dataFile, 'file')
+                bst_unzip(dataFile, bst_fullfile(dataDir, 'SubjectCMC'));
+                tutorial_coherence(dataDir);
+            end
+
+        case 'tutorial_dba'
+            dataFile = getTutorialFile(dataDir, 'TutorialDba.zip', bstUser, bstPwd);
+            if exist(dataFile, 'file')
+                tutorial_dba(dataFile);
+            end
+
+        case 'tutorial_ephys'
+            dataFile = getTutorialFile(dataDir, 'sample_ephys.zip', bstUser, bstPwd);
+            if exist(dataFile, 'file')
+                bst_unzip(dataFile, dataDir);
+                tutorial_ephys(dataDir);
+            end
+
+        case 'tutorial_epilepsy'
+            dataFile = getTutorialFile(dataDir, 'sample_epilepsy.zip', bstUser, bstPwd);
+            if exist(dataFile, 'file')
+                bst_unzip(dataFile, dataDir);
+                tutorial_epilepsy(dataDir);
+            end
+
+        case 'tutorial_epileptogenicity'
+            dataFile = getTutorialFile(dataDir, 'tutorial_epimap_bids.zip', bstUser, bstPwd);
+            if exist(dataFile, 'file')
+                bst_unzip(dataFile, dataDir);
+                tutorial_epileptogenicity(dataDir);
+            end
+
+        case 'tutorial_fem_charm'
+              infoStr = 'REQUIRES TO INSTALL SimNIBS';
+%             tmpDwnFile = mget(bstFtp, '/pub/tutorials/sample_fem.zip', tmpDir);
+%             bst_unzip(tmpDwnFile{:}, tmpDir);
+%             tutorial_fem_charm(tmpDir);
+
+        case 'tutorial_fem_tensors'
+              infoStr = 'REQUIRES TO BrainSuite';
+%             dataFile1 = bst_fullfile(dataDir, 'BrainSuiteTutorialSVReg.zip');
+%             if ~exist(dataFile1, 'file')
+%                 bst_websave(dataFile, 'http://brainsuite.org/WebTutorialData/BrainSuiteTutorialSVReg_Sept16.zip');
+%             end
+%             dataFile2 = bst_fullfile(dataDir, 'DWI.zip');
+%             if ~exist(dataFile2, 'file')
+%                 bst_websave(dataFile2, 'http://brainsuite.org/WebTutorialData/DWI_Feb15.zip');
+%             end
+%             if exist(dataFile1, 'file') && exist(dataFile2, 'file')
+%                 bst_unzip(dataFile1, dataDir);
+%                 bst_unzip(dataFile2, dataDir);
+%                 tutorial_fem_tensors(dataDir);
+%             end
+
+        case 'tutorial_frontiers2018'
+              infoStr = 'REQUIRES TO DOWNLOAD ~100GB https://openneuro.org/datasets/ds000117';
+%             tutorial_frontiers2018(tmpDir);
+
+        case 'tutorial_visual'
+              infoStr = 'REQUIRES TO DOWNLOAD ~100GB https://openneuro.org/datasets/ds000117';
+%             tutorial_visual(tmpDir);
+
+        case 'tutorial_hcp'
+              infoStr = 'REQUIRES TO DOWNLOAD ~20GB  HCP-MEG2 distribution: subject #175237';
+%             tutorial_hcp(tmpDir);
+
+        case 'tutorial_neuromag'
+            dataFile = getTutorialFile(dataDir, 'sample_neuromag.zip', bstUser, bstPwd);
+            if exist(dataFile, 'file')
+                bst_unzip(dataFile, dataDir);
+                tutorial_neuromag(dataDir);
+            end
+
+        case 'tutorial_omega'
+            infoStr = 'REQUIRES TO DOWNLOAD ~12GB  https://openneuro.org/datasets/ds000247';
+            % tutorial_omega(tmpDir);
+
+        case 'tutorial_phantom_ctf'
+            dataFile = getTutorialFile(dataDir, 'sample_phantom_ctf.zip', bstUser, bstPwd);
+            if exist(dataFile, 'file')
+                bst_unzip(dataFile, dataDir);
+                tutorial_phantom_ctf(dataDir);
+            end
+
+        case 'tutorial_phantom_elekta'
+            dataFile = getTutorialFile(dataDir, 'sample_phantom_elekta.zip', bstUser, bstPwd);
+            if exist(dataFile, 'file')
+                bst_unzip(dataFile, dataDir);
+                tutorial_phantom_elekta(dataDir);
+            end
+
+        case 'tutorial_practicalmeeg'
+            dataFile = getTutorialFile(dataDir, 'tutorial_practicalmeeg.zip', bstUser, bstPwd);
+            if exist(dataFile, 'file')
+                bst_unzip(dataFile, dataDir);
+                tutorial_practicalmeeg(bst_fullfile(dataDir, 'tutorial_practicalmeeg'));
+            end
+
+        case 'tutorial_raw'
+            dataFile = getTutorialFile(dataDir, 'sample_raw.zip', bstUser, bstPwd);
+            if exist(dataFile, 'file')
+                bst_unzip(dataFile, dataDir);
+                tutorial_raw(dataDir);
+            end
+
+        case 'tutorial_resting'
+            dataFile = getTutorialFile(dataDir, 'sample_resting.zip', bstUser, bstPwd);
+            if exist(dataFile, 'file')
+                bst_unzip(dataFile, dataDir);
+                tutorial_resting(dataDir);
+            end
+
+        case 'tutorial_simulations'
+            tutorial_simulations();
+
+        case 'tutorial_yokogawa'
+            dataFile = getTutorialFile(dataDir, 'sample_yokogawa.zip', bstUser, bstPwd);
+            if exist(dataFile, 'file')
+                bst_unzip(dataFile, dataDir);
+                tutorial_yokogawa(dataDir);
+            end
+    end
+
+    % Get report (available if tutorial was run)
+    [~, ReportFile] = bst_report('GetReport', 'last');
+    % Was the tutorial run?
+    wasRun = ~isempty(ReportFile);
+    % Generate not-executed report
+    if ~wasRun
+        tmp = struct('Comment', tutoriallName); % Dummy struct to give name to report
+        bst_report('Reset');
+        bst_report('Add', 'start', [], [], tmp);
+        bst_report('Info', '', '', sprintf('"%s" was not executed', tutoriallName));
+        bst_report('Info', '', '', infoStr);
+        ReportFile = bst_report('Save');
+    end
+    % === Save report file
+    if ~isempty(reportDir) && ~isempty(ReportFile)
+        [~, baseName] = bst_fileparts(ReportFile);
+        bst_report('Export', ReportFile, bst_fullfile(reportDir, [baseName, '.html']));
+    end
+
+    % === Report email
+    if ~isempty(bstUser)
+        % Tutorial info
+        tutorialInfo = sprintf('Tutorial: "%s"', tutoriallName);
+        % Hostname and OS
+        [~, hostName] = system('hostname');
+        hostInfo = sprintf('Host: "%s" with %s', strtrim(hostName), bst_get('OsName'));
+        % Matlab info
+        matlabInfo = sprintf('Matlab: %s', bst_get('MatlabReleaseName'));
+        % Brainstorm info
+        bstVersion = bst_get('Version');
+        bstVariant = 'source';
+        if bst_iscompiled()
+            bstVariant = 'standalone';
+        end
+        bstInfo = sprintf('BST: %s (%s)', bstVersion.Version, bstVariant);
+        % Status
+        if wasRun
+            statusInfo = '[Complete]';
+        else
+            statusInfo = '[Not exec]';
+        end
+        % Subject string
+        strSubject = [statusInfo, ' ' tutorialInfo, ', ' hostInfo, ', ' matlabInfo, ', ', bstInfo];
+
+        % Process: Send report by email
+        bst_process('CallProcess', 'process_report_email', [], [], ...
+            'username',   bstUser, ...
+            'cc',         '', ...
+            'subject',    strSubject, ...
+            'reportfile', ReportFile, ...
+            'full',       1);
+    end
+end
 
 % Stop Brainstorm
-brainstorm stop
-% Quit Matlab
-exit
+if stopBstAtEnd
+    brainstorm stop
+end
+end
+
+%% ===== GET FILE =====
+function dataFullFile = getTutorialFile(dataDir, dataFile, bstUserName, bstPwd)
+    % Prepare the data file to run the tutorial
+    % Download if needed and credentials are provided
+    dataFullFile = bst_fullfile(dataDir, dataFile);
+    % Try to download if file does not exist
+    if ~exist(dataFullFile, 'file')
+        if ~isempty(bstUserName) && ~isempty(bstPwd)
+            dwnUrl = sprintf('http://neuroimage.usc.edu/bst/download.php?file=%s&user=%s&mdp=%s', ...
+                             urlencode(dataFile), urlencode(bstUserName), urlencode(bstPwd));
+            dataFullFile = bst_fullfile(dataDir, dataFile);
+            errMsg = bst_websave(dataFullFile, dwnUrl);
+            % Return if error
+            if ~isempty(errMsg) || ~exist(dataFullFile, 'file')
+                dataFullFile = '';
+                return
+            end
+        else
+            dataFullFile = '';
+            return
+        end
+    end
+    % Check size, if less than 50 bytes, error with the downloaded file
+    d = dir(dataFullFile);
+    if d.bytes < 50
+        dataFullFile = '';
+        return
+    end
+end
